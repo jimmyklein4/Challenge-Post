3b916fffdbba9fb4133be376f28ccdcc7e91deef
Jimmy Klein <jimmy.klein4@gmail.com>
2016-02-27 16:58:13 -0500

    init

---
+++ app/src/main/java/org/tudev/challengepost/LoginActivity.java
@@ -0,0 +1,21 @@
+package org.tudev.challengepost;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by jimmyklein on 2/27/16.
+ */
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+
+    //TODO: Add link to login button
+    public void loginPressed(){
+
+    }
+}
