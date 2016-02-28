39556a7c6ca61f43a01e7e2acb67840487684d28
Jimmy Klein <jimmy.klein4@gmail.com>
2016-02-27 22:30:36 -0500

    Lists populate

--- app/src/main/java/org/tudev/challengepost/MainActivity.java
+++ app/src/main/java/org/tudev/challengepost/MainActivity.java
@@ -4,13 +4,28 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+
+    //TODO: Get these values from the server
+    private String[] challenges = {"Challenge1","Challenge2","Challenge3"};
+    private String[] feed = {"Feed1","Feed2","Feed3"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter challengeAdapter = new ArrayAdapter<>(this, R.layout.challenge_listview, challenges);
+        ArrayAdapter feedAdapter = new ArrayAdapter<>(this, R.layout.feed_listview, feed);
+
+        ListView challengeListView = (ListView) findViewById(R.id.listChallenges);
+        ListView feedListView = (ListView) findViewById(R.id.listFeed);
+
+        challengeListView.setAdapter(challengeAdapter);
+        feedListView.setAdapter(feedAdapter);
     }
 
     @Override
