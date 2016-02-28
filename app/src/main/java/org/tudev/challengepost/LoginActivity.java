885bcc20c2fce35ead493d63f87747a47cc5b2b2
Jimmy Klein <jimmy.klein4@gmail.com>
2016-02-27 20:21:41 -0500

    Added stuff to registration

--- app/src/main/java/org/tudev/challengepost/LoginActivity.java
+++ app/src/main/java/org/tudev/challengepost/LoginActivity.java
@@ -4,27 +4,45 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by jimmyklein on 2/27/16.
  */
 public class LoginActivity extends Activity {
 
+    private EditText username;
+    private EditText password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        username = (EditText) findViewById(R.id.edit_username);
+        password = (EditText) findViewById(R.id.edit_password);
+
+        Intent intent = getIntent();
+        if(intent!=null){
+            username.setText(intent.getStringExtra(RegistrationActivity.EXTRA_USERNAME));
+        }
     }
 
     //TODO: Handle Login Properly
     public void sendLogin(View view){
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        if((username.getText()!=null || password.getText()!=null)) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            Toast.makeText(this, "Please enter a username and password", Toast.LENGTH_LONG).show();
+        }
     }
 
     public void sendRegister(View view){
         Intent rIntent = new Intent(this, RegistrationActivity.class);
         startActivity(rIntent);
+        finish();
     }
 }
--- app/src/main/java/org/tudev/challengepost/RegistrationActivity.java
+++ app/src/main/java/org/tudev/challengepost/RegistrationActivity.java
@@ -1,7 +1,11 @@
 package org.tudev.challengepost;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by jimmyklein on 2/27/16.
@@ -10,9 +14,30 @@ public class RegistrationActivity extends Activity {
 
     //TODO: Implement registration
 
+    public final static String EXTRA_USERNAME = "org.tudev.challengepost.USERNAME";
+
+    private EditText username;
+    private EditText password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration_acitivty);
+        username = (EditText) findViewById(R.id.edit_username);
+        password = (EditText) findViewById(R.id.edit_password);
+    }
+
+    public void sendRegister(View view){
+        if(!(username.getText()==null || password.getText()==null)){
+            //TODO: Send registration to server
+            Intent intent = new Intent(this, LoginActivity.class);
+            intent.putExtra(EXTRA_USERNAME, username.getText().toString());
+            startActivity(intent);
+            finish();
+
+        } else {
+            //User did not both a username and password
+            Toast.makeText(this, "Please enter a username and password", Toast.LENGTH_LONG).show();
+        }
     }
 }
